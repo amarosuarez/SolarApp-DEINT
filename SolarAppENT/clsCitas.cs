0d326f6538320b7fdc7a367919d36d397174e863
amarosuarez <amaro.suarez@iesnervion.es>
2024-11-05 21:37:05 +0100

    Agregar archivos de proyecto.

---
+++ SolarAppENT/clsCitas.cs
@@ -0,0 +1,26 @@
+﻿namespace SolarAppENT
+{
+    public class clsCitas
+    {
+        #region Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public clsCliente Cliente { get; set; }
+        #endregion
+
+        #region Constructores
+        public clsCitas() { }
+
+        public clsCitas(int id, string nombre, string ubicacion, string fecha, clsCliente cliente)
+        {
+            Id = id;
+            Nombre = nombre;
+            Fecha = DateTime.Parse(fecha);
+            Cliente = cliente;
+        }
+        #endregion
+    }
+}
---
+++ SolarAppENT/clsCliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarAppENT
+{
+    public class clsCliente
+    {
+        #region Atributos
+        public int Id {  get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Direccion {  get; set; }
+
+        public int Telefono {
+            get { return Telefono; }
+            set { 
+                if (value.ToString().Length == 9)
+                    Telefono = value;
+            }
+        }
+        #endregion
+
+        #region Constructores
+        public clsCliente() { }
+
+        public clsCliente(int id, string nombre, string direccion, int telefono)
+        {
+            Id = id;
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+        } 
+        #endregion
+
+    }
+}
